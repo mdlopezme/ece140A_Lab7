d7b2b756f88e6a5ccfd5074bdbf180b9057d395f
Moises Lopez <mdlopezme@gmail.com>
2022-02-28 13:34:47 -0800

    fetch image

--- Challenges/public/rest.js
+++ Challenges/public/rest.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     dropdown = document.getElementById('dropdown_selection');
+    car_image = document.getElementById('car_image')
+
     fetch('/names')
         .then(response=>response.json())
         .then(function(response){
             for(index in response){
                 var opt = document.createElement('option');
-                opt.value = index;
+                opt.value = response[index];
                 opt.innerHTML = response[index];
                 dropdown.appendChild(opt);
             }
         })
+        .then( function (){
+            console.log(dropdown.value)
+            car_image.src = 'images/' + dropdown.value
+            car_image.alt = dropdown.value
+        }
+        )
 });
